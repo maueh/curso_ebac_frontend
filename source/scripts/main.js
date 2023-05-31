89c133081011102c93487d375eceee0e1d0c07db
Marcel <uehdev@gmail.com>
2023-05-31 19:50:16 -0300

    Commit exercicio BEM

--- source/scripts/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    testeGulp();
-});
-
-function testeGulp() {
-    alert("O arquivo Javascript foi compilado pelo plugin do Grunt");
-    console.log("Compilando JS com o Grunt");
-}
