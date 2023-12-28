05677ba7a09cb774e0f28b8ce69c201b8bb039af
Marcel <uehdev@gmail.com>
2023-12-28 07:02:41 -0300

    Commit aula ts

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+// Função de multiplicação de dois argumentos recebidos
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+// Função de saudação
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+let resultadoMultiplicacao = multiplicacao(3, 9);
+console.log(resultadoMultiplicacao);
+let minhaSaudacao = saudacao("Joselito");
+console.log(minhaSaudacao);
